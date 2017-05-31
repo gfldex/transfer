76a3f65e1f79840a95182faf708185ff4158e977
Wenzel P. P. Peppmeyer <wenzel.peppmeyer@gmx.de>
2017-05-31 13:52:07 +0200

    add id()

--- snippet.js
+++ snippet.js
@@ -4,3 +4,27 @@ document.querySelectorAll("input[type=checkbox]").forEach((e) => {
     })
 });
 
+function id(){ 
+    var targets = Array.apply(null, arguments).map((name) => { return document.getElementById(name) });
+
+    function list_proxy(targets) {
+        return new Proxy({}, {
+            get: (target, name) => {
+                if (name == "__dump__") {
+                    return targets;
+                }
+                return list_proxy(targets.map((target) => { return target[name] }));
+            },
+            set: (target, name, value) => {
+                targets.forEach((target) => { target[name] = value; });
+                return value;
+            },
+            apply: (target, that, args) => {
+                return targets.map((target) => { return target.apply(that, args) });
+            }
+        });
+    }
+
+    return list_proxy(targets);
+}
+
